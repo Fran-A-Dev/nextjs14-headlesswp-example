5e1a0ddc8eef4481ed62c0d42c81d65fbf08adca
Fran Agulto <francis.agulto@wpengine.com>
2023-08-25 10:52:03 -0500

    added create post form

---
+++ app/post/create/page.jsx
@@ -0,0 +1,10 @@
+import CreatePost from "./CreatePost";
+
+export default async function Page() {
+  return (
+    <main>
+      <h2 className="text-center">Create Post</h2>
+      <CreatePost />
+    </main>
+  );
+}
