9f3477491da5d78399c6260378c4468dfed09187
Fran Agulto <francis.agulto@wpengine.com>
2023-09-06 08:20:24 -0500

    added login and auth draft capabilities

--- app/login/AuthForm.jsx
+++ app/login/AuthForm.jsx
@@ -1,16 +1,14 @@
 "use client";
-import dynamic from "next/dynamic";
 import { useState } from "react";
-
-const QuillEditor = dynamic(() => import("react-quill"), { ssr: false });
-import "react-quill/dist/quill.snow.css";
 
 const AuthForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [jwt, setJwt] = useState("");
   const [posts, setPosts] = useState([]);
+  const [title, setTitle] = useState(""); // Add the title state variable
   const [content, setContent] = useState("");
+  const [loginError, setLoginError] = useState(null);
 
   const loginAndFetch = async (e) => {
     e.preventDefault();
@@ -22,21 +20,21 @@ const AuthForm = () => {
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           query: `
-            mutation LoginUser {
-              login( input: {
-                clientMutationId: "uniqueId",
-                username: "${username}",
-                password: "${password}"
-              }){
-                authToken
-                user {
-                  id
-                  databaseId
-                  name
+              mutation LoginUser {
+                login( input: {
+                  clientMutationId: "uniqueId",
+                  username: "${username}",
+                  password: "${password}"
+                }){
+                  authToken
+                  user {
+                    id
+                    databaseId
+                    name
+                  }
                 }
               }
-            }
-          `,
+            `,
         }),
       });
 
@@ -57,15 +55,15 @@ const AuthForm = () => {
             },
             body: JSON.stringify({
               query: `
-              query GetAllPosts {
-                posts(where: { status: DRAFT }) {
-                  nodes {
-                    title
-                    content
+                query GetAllPosts {
+                  posts(where: { status: DRAFT }) {
+                    nodes {
+                      title
+                      content
+                    }
                   }
                 }
-              }
-            `,
+              `,
             }),
           }
         );
@@ -99,7 +97,7 @@ const AuthForm = () => {
               }
             `,
           variables: {
-            title: "Draft Authentication Post",
+            title: title,
             content: content,
           },
         }),
@@ -135,17 +133,14 @@ const AuthForm = () => {
         />
         <button type="submit">Login</button>
       </form>
+      {loginError && <p className="text-red-500">{loginError}</p>}
       <section className="mb-8">
         <h3 className="font-semibold mb-2 text-primary">Draft Posts</h3>
         <ul>
           {posts.map((post) => (
-            <li key={post.title}>
+            <li key={post.title} className="card">
               <h4 className="text-lg font-medium">{post.title}</h4>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: post.content,
-                }}
-              />
+              <p dangerouslySetInnerHTML={{ __html: post.content }} />
             </li>
           ))}
         </ul>
@@ -153,11 +148,28 @@ const AuthForm = () => {
 
       {jwt && (
         <section>
-          <h3 className="font-semibold mb-2 text-primary">Create Post</h3>
-          <QuillEditor
-            theme="snow"
+          <div className="mb-4">
+            <h3 className="font-semibold mb-2 text-primary">
+              Create A Draft Post
+            </h3>
+            <label htmlFor="titleInput">Title</label>
+            <input
+              type="text"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              id="titleInput"
+              className="w-full rounded border border-gray-300 p-2"
+            />
+          </div>
+          <label htmlFor="contentInput">Content</label>
+          <textarea
+            name="content"
             value={content}
-            onChange={(newContent) => setContent(newContent)}
+            onChange={(e) => setContent(e.target.value)}
+            id="contentInput"
+            className="block w-full rounded border border-gray-300 p-2"
+            rows="4"
           />
           <br />
           <button
@@ -165,7 +177,7 @@ const AuthForm = () => {
             className="bg-green-500 hover:bg-green-600 text-white font-semibold px-4 py-2 rounded"
             onClick={createDraftPost}
           >
-            Create New Post
+            Create A New Draft Post
           </button>
         </section>
       )}
